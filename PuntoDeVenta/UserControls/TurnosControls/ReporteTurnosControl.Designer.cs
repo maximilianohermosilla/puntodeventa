1443b6fd9472ddaede549ff53faa3285ea95544a
maximiliano_hermosilla <maximiliano_hermosilla@hotmail.com>
2025-12-01 00:08:10 -0300

    Reporte Turnos User Control

---
+++ PuntoDeVenta/UserControls/TurnosControls/ReporteTurnosControl.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace PuntoDeVenta.UserControls.TurnosControls
+{
+    partial class ReporteTurnosControl
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReporteTurnosControl));
+            panelMain = new Panel();
+            panelDataGrid = new Panel();
+            labelSinResultados = new Label();
+            dvMovimientos = new DataGridView();
+            panelHeader = new Panel();
+            linkLabelMes = new LinkLabel();
+            linkLabelSemana = new LinkLabel();
+            linkLabelHoy = new LinkLabel();
+            labelTotal = new Label();
+            btnExportar = new Button();
+            btnBuscar = new Button();
+            labelHasta = new Label();
+            labelDesde = new Label();
+            dateHasta = new DateTimePicker();
+            dateDesde = new DateTimePicker();
+            labelTitle = new Label();
+            panelMain.SuspendLayout();
+            panelDataGrid.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dvMovimientos).BeginInit();
+            panelHeader.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelMain
+            // 
+            panelMain.BackColor = Color.FromArgb(49, 66, 82);
+            panelMain.Controls.Add(panelDataGrid);
+            panelMain.Controls.Add(panelHeader);
+            panelMain.Dock = DockStyle.Fill;
+            panelMain.ForeColor = SystemColors.ButtonHighlight;
+            panelMain.Location = new Point(0, 0);
+            panelMain.Name = "panelMain";
+            panelMain.Size = new Size(1447, 800);
+            panelMain.TabIndex = 0;
+            // 
+            // panelDataGrid
+            // 
+            panelDataGrid.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            panelDataGrid.Controls.Add(labelSinResultados);
+            panelDataGrid.Controls.Add(dvMovimientos);
+            panelDataGrid.Dock = DockStyle.Top;
+            panelDataGrid.Location = new Point(0, 126);
+            panelDataGrid.Name = "panelDataGrid";
+            panelDataGrid.Size = new Size(1447, 251);
+            panelDataGrid.TabIndex = 25;
+            // 
+            // labelSinResultados
+            // 
+            labelSinResultados.AutoSize = true;
+            labelSinResultados.Font = new Font("Segoe UI", 16F);
+            labelSinResultados.Location = new Point(6, 13);
+            labelSinResultados.Name = "labelSinResultados";
+            labelSinResultados.Size = new Size(298, 30);
+            labelSinResultados.TabIndex = 24;
+            labelSinResultados.Text = "No se encontraron resultados";
+            labelSinResultados.Visible = false;
+            // 
+            // dvMovimientos
+            // 
+            dvMovimientos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dvMovimientos.BackgroundColor = Color.FromArgb(49, 66, 82);
+            dvMovimientos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dvMovimientos.Dock = DockStyle.Fill;
+            dvMovimientos.Location = new Point(0, 0);
+            dvMovimientos.Name = "dvMovimientos";
+            dvMovimientos.RowTemplate.DefaultCellStyle.ForeColor = Color.Black;
+            dvMovimientos.Size = new Size(1447, 251);
+            dvMovimientos.TabIndex = 23;
+            // 
+            // panelHeader
+            // 
+            panelHeader.Controls.Add(linkLabelMes);
+            panelHeader.Controls.Add(linkLabelSemana);
+            panelHeader.Controls.Add(linkLabelHoy);
+            panelHeader.Controls.Add(labelTotal);
+            panelHeader.Controls.Add(btnExportar);
+            panelHeader.Controls.Add(btnBuscar);
+            panelHeader.Controls.Add(labelHasta);
+            panelHeader.Controls.Add(labelDesde);
+            panelHeader.Controls.Add(dateHasta);
+            panelHeader.Controls.Add(dateDesde);
+            panelHeader.Controls.Add(labelTitle);
+            panelHeader.Dock = DockStyle.Top;
+            panelHeader.Location = new Point(0, 0);
+            panelHeader.Name = "panelHeader";
+            panelHeader.Size = new Size(1447, 126);
+            panelHeader.TabIndex = 28;
+            // 
+            // linkLabelMes
+            // 
+            linkLabelMes.AutoSize = true;
+            linkLabelMes.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            linkLabelMes.LinkColor = Color.FromArgb(128, 128, 255);
+            linkLabelMes.Location = new Point(226, 45);
+            linkLabelMes.Name = "linkLabelMes";
+            linkLabelMes.Size = new Size(88, 21);
+            linkLabelMes.TabIndex = 27;
+            linkLabelMes.TabStop = true;
+            linkLabelMes.Text = "Mes actual";
+            linkLabelMes.LinkClicked += linkLabelMes_LinkClicked;
+            // 
+            // linkLabelSemana
+            // 
+            linkLabelSemana.AutoSize = true;
+            linkLabelSemana.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            linkLabelSemana.LinkColor = Color.FromArgb(128, 128, 255);
+            linkLabelSemana.Location = new Point(81, 45);
+            linkLabelSemana.Name = "linkLabelSemana";
+            linkLabelSemana.Size = new Size(116, 21);
+            linkLabelSemana.TabIndex = 26;
+            linkLabelSemana.TabStop = true;
+            linkLabelSemana.Text = "Última semana";
+            linkLabelSemana.LinkClicked += linkLabelSemana_LinkClicked;
+            // 
+            // linkLabelHoy
+            // 
+            linkLabelHoy.AutoSize = true;
+            linkLabelHoy.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            linkLabelHoy.LinkColor = Color.FromArgb(128, 128, 255);
+            linkLabelHoy.Location = new Point(10, 45);
+            linkLabelHoy.Name = "linkLabelHoy";
+            linkLabelHoy.Size = new Size(40, 21);
+            linkLabelHoy.TabIndex = 25;
+            linkLabelHoy.TabStop = true;
+            linkLabelHoy.Text = "Hoy";
+            linkLabelHoy.LinkClicked += linkLabelHoy_LinkClicked;
+            // 
+            // labelTotal
+            // 
+            labelTotal.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            labelTotal.AutoSize = true;
+            labelTotal.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelTotal.ForeColor = SystemColors.ButtonHighlight;
+            labelTotal.Location = new Point(1256, 87);
+            labelTotal.Name = "labelTotal";
+            labelTotal.Size = new Size(97, 25);
+            labelTotal.TabIndex = 24;
+            labelTotal.Text = "TOTAL: $0";
+            // 
+            // btnExportar
+            // 
+            btnExportar.BackColor = Color.FromArgb(0, 80, 200);
+            btnExportar.BackgroundImageLayout = ImageLayout.None;
+            btnExportar.FlatAppearance.BorderColor = Color.Silver;
+            btnExportar.FlatAppearance.BorderSize = 0;
+            btnExportar.FlatAppearance.MouseDownBackColor = Color.Navy;
+            btnExportar.FlatAppearance.MouseOverBackColor = Color.Teal;
+            btnExportar.FlatStyle = FlatStyle.Flat;
+            btnExportar.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnExportar.ForeColor = SystemColors.ButtonHighlight;
+            btnExportar.Image = (Image)resources.GetObject("btnExportar.Image");
+            btnExportar.Location = new Point(623, 85);
+            btnExportar.Name = "btnExportar";
+            btnExportar.Size = new Size(114, 35);
+            btnExportar.TabIndex = 23;
+            btnExportar.Text = "Exportar";
+            btnExportar.TextAlign = ContentAlignment.MiddleRight;
+            btnExportar.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnExportar.UseVisualStyleBackColor = false;
+            btnExportar.Click += btnExportar_Click;
+            // 
+            // btnBuscar
+            // 
+            btnBuscar.BackColor = Color.FromArgb(0, 80, 200);
+            btnBuscar.BackgroundImageLayout = ImageLayout.None;
+            btnBuscar.FlatAppearance.BorderColor = Color.Silver;
+            btnBuscar.FlatAppearance.BorderSize = 0;
+            btnBuscar.FlatAppearance.MouseDownBackColor = Color.Navy;
+            btnBuscar.FlatAppearance.MouseOverBackColor = Color.Teal;
+            btnBuscar.FlatStyle = FlatStyle.Flat;
+            btnBuscar.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnBuscar.ForeColor = SystemColors.ButtonHighlight;
+            btnBuscar.Image = (Image)resources.GetObject("btnBuscar.Image");
+            btnBuscar.Location = new Point(511, 85);
+            btnBuscar.Name = "btnBuscar";
+            btnBuscar.Size = new Size(93, 35);
+            btnBuscar.TabIndex = 22;
+            btnBuscar.Text = "Buscar";
+            btnBuscar.TextAlign = ContentAlignment.MiddleRight;
+            btnBuscar.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnBuscar.UseVisualStyleBackColor = false;
+            btnBuscar.Click += btnBuscar_Click;
+            // 
+            // labelHasta
+            // 
+            labelHasta.AutoSize = true;
+            labelHasta.Font = new Font("Segoe UI", 10F);
+            labelHasta.Location = new Point(261, 75);
+            labelHasta.Name = "labelHasta";
+            labelHasta.Size = new Size(47, 19);
+            labelHasta.TabIndex = 21;
+            labelHasta.Text = "Hasta:";
+            // 
+            // labelDesde
+            // 
+            labelDesde.AutoSize = true;
+            labelDesde.Font = new Font("Segoe UI", 10F);
+            labelDesde.Location = new Point(10, 75);
+            labelDesde.Name = "labelDesde";
+            labelDesde.Size = new Size(54, 19);
+            labelDesde.TabIndex = 20;
+            labelDesde.Text = "Desde: ";
+            // 
+            // dateHasta
+            // 
+            dateHasta.Location = new Point(261, 97);
+            dateHasta.Name = "dateHasta";
+            dateHasta.Size = new Size(227, 23);
+            dateHasta.TabIndex = 4;
+            // 
+            // dateDesde
+            // 
+            dateDesde.Location = new Point(10, 97);
+            dateDesde.Name = "dateDesde";
+            dateDesde.Size = new Size(227, 23);
+            dateDesde.TabIndex = 3;
+            // 
+            // labelTitle
+            // 
+            labelTitle.AutoSize = true;
+            labelTitle.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelTitle.ForeColor = SystemColors.ButtonHighlight;
+            labelTitle.Location = new Point(10, 10);
+            labelTitle.Name = "labelTitle";
+            labelTitle.Size = new Size(199, 25);
+            labelTitle.TabIndex = 2;
+            labelTitle.Text = "REPORTE DE TURNOS";
+            // 
+            // ReporteTurnosControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLight;
+            Controls.Add(panelMain);
+            Name = "ReporteTurnosControl";
+            Size = new Size(1447, 800);
+            panelMain.ResumeLayout(false);
+            panelDataGrid.ResumeLayout(false);
+            panelDataGrid.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dvMovimientos).EndInit();
+            panelHeader.ResumeLayout(false);
+            panelHeader.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelMain;
+        private Label labelTitle;
+        private DateTimePicker dateHasta;
+        private DateTimePicker dateDesde;
+        private Label labelHasta;
+        private Label labelDesde;
+        private DataGridView dvMovimientos;
+        public Button btnBuscar;
+        private Panel panelDataGrid;
+        private Label labelSinResultados;
+        private Panel panelHeader;
+        public Button btnExportar;
+        private Label labelTotal;
+        private LinkLabel linkLabelHoy;
+        private LinkLabel linkLabelMes;
+        private LinkLabel linkLabelSemana;
+    }
+}
---
+++ PuntoDeVenta/UserControls/TurnosControls/ReporteTurnosControl.cs
@@ -0,0 +1,142 @@
+﻿using PuntoDeVenta.AccessData;
+using PuntoDeVenta.Application.DTO;
+using PuntoDeVenta.Application.Interfaces;
+using PuntoDeVenta.Application.Services;
+using PuntoDeVenta.Enum;
+using PuntoDeVenta.Helpers;
+using System.Data;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PuntoDeVenta.UserControls.TurnosControls
+{
+    public partial class ReporteTurnosControl : UserControl
+    {
+        private static PuntoDeVentaDbContext _context = new PuntoDeVentaDbContext();
+        private readonly IProductoMovimientoService _productoMovimientoService;
+
+        private List<ProductoMovimientoResponse> _productoMovimientos = new List<ProductoMovimientoResponse>();
+
+        public ReporteTurnosControl()
+        {
+            _productoMovimientoService = new ProductoMovimientoService(_context);
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            _ = GetAllMovimientos();
+        }
+
+        public async Task GetAllMovimientos()
+        {
+            try
+            {
+                var desde = new DateTime(dateDesde.Value.Year, dateDesde.Value.Month, dateDesde.Value.Day, 0, 0, 0);
+                var hasta = new DateTime(dateHasta.Value.Year, dateHasta.Value.Month, dateHasta.Value.Day, 23, 59, 0);
+                var response = await _productoMovimientoService.GetAllByFechaAndTipoMovimiento(desde, hasta, (int)TipoMovimientoEnum.Salida);
+
+                if (response != null && response.success)
+                {
+                    _productoMovimientos = response.response!;
+                    SetearMovimientos(_productoMovimientos);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void SetearMovimientos(List<ProductoMovimientoResponse> movimientos)
+        {
+            if (movimientos != null && movimientos.Any())
+            {
+                var listaMovimientos = movimientos.Select(x => new
+                {
+                    Codigo = x.Producto != null ? x.Producto!.Codigo : "",
+                    Producto = x.Descripcion ?? "",
+                    Categoria = x.Producto != null && x.Producto?.CategoriaProducto != null ? x.Producto?.CategoriaProducto?.Descripcion : "Producto Común",
+                    x.Cantidad,
+                    x.Valor,
+                    FormaPago = x.FormaPago != null ? x.FormaPago.Descripcion : "",
+                    TipoMovimiento = x.TipoMovimiento.Descripcion ?? "",
+                    Usuario = x.Usuario.User ?? "",
+                    x.Fecha
+                })!.ToList();
+
+                dvMovimientos.DataSource = null;
+                dvMovimientos.DataSource = listaMovimientos;
+                dvMovimientos.Refresh();
+                dvMovimientos.Invalidate();
+
+                dvMovimientos.Visible = true;
+                labelSinResultados.Visible = false;
+                labelTotal.Text = $"TOTAL: ${movimientos.Sum(p => p.Valor)}";
+                labelTotal.Visible = true;
+            }
+            else
+            {
+                dvMovimientos.Visible = false;
+                labelSinResultados.Visible = true;
+                labelTotal.Visible = false;
+            }
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFolderPath = folderBrowserDialog1.SelectedPath;
+                DataTable dt = new DataTable();
+
+                foreach (DataGridViewColumn column in dvMovimientos.Columns)
+                {
+                    dt.Columns.Add(column.HeaderText, typeof(object));
+                }
+
+                // Add rows to the DataTable based on DataGridView rows
+                foreach (DataGridViewRow row in dvMovimientos.Rows)
+                {
+                    if (row.IsNewRow) continue;
+
+                    DataRow dr = dt.NewRow();
+                    for (int i = 0; i < dvMovimientos.Columns.Count; i++)
+                    {
+                        dr[i] = row.Cells[i].Value;
+                    }
+                    dt.Rows.Add(dr);
+                }
+
+                string fecha = dateDesde.Value.ToString("yyyy-MM-dd") == dateHasta.Value.ToString("yyyy-MM-dd") ? dateDesde.Value.ToString("yyyy-MM-dd") : $"{dateDesde.Value.ToString("yyyy-MM-dd")}-{dateHasta.Value.ToString("yyyy-MM-dd")}";
+                bool exportado = ExportCsvHelper.ExportCsv(dt, $"ReporteVentas_{fecha}", selectedFolderPath);
+
+                string toastTipo = exportado ? "SUCCESS" : "ERROR";
+                ToastForm toast = new ToastForm(toastTipo, exportado ? "Exportación finalizada." : "Ocurrió un error en la exportación.", this.FindForm()!);
+                toast.Show();
+            }
+        }
+
+        private void linkLabelHoy_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            dateDesde.Value = DateTime.Now;
+            dateHasta.Value = DateTime.Now;
+            _ = GetAllMovimientos();
+        }
+
+        private void linkLabelSemana_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            dateDesde.Value = DateTime.Now.AddDays(-7);
+            dateHasta.Value = DateTime.Now;
+            _ = GetAllMovimientos();
+        }
+
+        private void linkLabelMes_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            dateDesde.Value = new DateTime(dateDesde.Value.Year, dateDesde.Value.Month, 1, 0, 0, 0);
+            dateHasta.Value = DateTime.Now;
+            _ = GetAllMovimientos();
+        }
+    }
+}
